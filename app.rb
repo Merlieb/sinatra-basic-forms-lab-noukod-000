fba8bd7886af92f489500eebe019ba1216eb74fc
Merline Belizaire <merline@noukod.com>
2019-01-15 19:26:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,14 +5,16 @@ get '/' do
   erb :index
 end
 
-get '/new' do
-  erb :create_puppy
-end
+
 
 post '/' do
   @name=@params[:name]
   @breed=@params[:breed]
   @age=@params[:age]
   erb :display_puppy
+end
+
+get '/new' do
+  erb :create_puppy
 end
 end
