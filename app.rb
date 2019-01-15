2c994542a8a1d9bbbb9ae9e7905dee3687962aba
Merline Belizaire <merline@noukod.com>
2019-01-15 19:38:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,18 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-get '/new' do
-  erb :create_puppy
-end
+  post '/' do
+    @name=params[:name]
+    @age=params[:age]
+    @breed=params[:breed]
+    erb :display_puppy
+  end
 
-post '/' do
-  @name=@params[:name]
-  @breed=@params[:breed]
-  @age=@params[:age]
-  erb :display_puppy
-end
+  get '/new' do
+    erb :create_puppy
+  end
+
 end
